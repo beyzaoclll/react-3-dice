1797adf24f2d60f50c764ee12dd43ec397bc63c1
beyzaoclll <byzocal34@gmail.com>
2024-06-23 19:35:36 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './Header.js';
+import RollDice from './RollDice.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <RollDice/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'; // Eğer stil eklemek isterseniz
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo">HAYDİ ÇEViR</div>
+    </header>
+  );
+};
+
+export default Header;
